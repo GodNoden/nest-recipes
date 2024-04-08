d93999064343c6b6cb2a82b97cd68db0ea5287ac
nqueztor <nqueztor-at-013810269802>
2024-04-08 21:11:29 +0000

    added dockerfile and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { cors } from 'cors';
+
+
 
 async function bootstrap() {
+
+  const cors = require("cors");
+  const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+
+  const port = 8080;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(cors(corsOptions));
+  await app.listen(port);
+  console.log("http://localhost:" + port);
+
 }
 bootstrap();
